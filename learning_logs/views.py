0be7e9c1d9051e5df1f8abc3b0f89479735f8617
kuma-babylontech <kuma@babylon-tech.com>
2024-10-21 14:58:14 +0900

    トピック一覧ページを追加

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
     """学習ノートのホームページ"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """全てのトピックを表示するページ"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)

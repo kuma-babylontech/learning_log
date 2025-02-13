40456e8bd4617bf45873525cedc583bb959399f1
kuma <kuma@babylon-tech.com>
2025-02-13 17:20:57 +0900

    ホームページ以外のページにアクセス制限を追加

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -18,6 +18,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """1つのトピックとそれに関連する全てのエントリを表示する"""
     topic = Topic.objects.get(id=topic_id)
@@ -26,6 +27,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """新しいトピックを追加する"""
     if request.method != 'POST':
@@ -42,6 +44,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """特定のトピックに新しいエントリを追加する"""
     topic = Topic.objects.get(id=topic_id)
@@ -62,6 +65,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """既存のエントリを編集する"""
     entry = Entry.objects.get(id=entry_id)
